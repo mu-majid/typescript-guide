10a701b15f5389c5c314012cec24b5b8e1862efc
Muhammad Majid <mmajidkamel@gmail.com>
2021-09-15 21:21:27 +0200

    Update UserEdit.ts

--- 5-web/src/views/UserEdit.ts
+++ 5-web/src/views/UserEdit.ts
@@ -12,6 +12,7 @@ export class UserEdit extends View<User, UserProps> {
     };
   }
 
+  
   // Handling Nested Views Manually here.
   onRender(): void {
     new UserShow(this.regions.userShow, this.model).render();
