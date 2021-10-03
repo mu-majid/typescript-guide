fff06c1909daf53ada5f405a199e4e46e0434464
Muhammad Majid <mmajidkamel@gmail.com>
2021-10-03 12:06:59 +0200

    Update CollectionView.ts

--- 5-web/src/views/CollectionView.ts
+++ 5-web/src/views/CollectionView.ts
@@ -1,6 +1,7 @@
 import { Collection } from '../models/Collection';
 
 export abstract class CollectionView<T, K> {
+  
   constructor(public parent: Element, public collection: Collection<T, K>) {}
 
   abstract renderItem(model: T, itemParent: Element): void;
